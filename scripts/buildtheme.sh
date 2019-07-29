586e560055b3bed40cecbfb542b31af3e5251895
Filip Haglund <filip.l.haglund@gmail.com>
2019-07-29 10:29:27 +0200

    :sparkles: Add support for deplying composer modules

--- scripts/buildtheme.sh
+++ scripts/buildtheme.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+composer install --no-dev
 mkdir -p build/wp-content/themes/akademi
 cp -R src/php/* build/wp-content/themes/akademi
 cp -R src/meta/wp-theme-meta/* build/wp-content/themes/akademi
 cp -R src/plugins/* build/wp-content/plugins
+cp -R vendor build/wp-content/themes/akademi
 cp -R src/webroot/* build/ 2>/dev/null || :
 cp src/wp-config.php build
+composer install
