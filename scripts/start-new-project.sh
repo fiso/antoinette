f3a87f5e00c0e918b2c7afb52abc1ed4e46d8cd7
Filip Haglund <filip.l.haglund@gmail.com>
2019-08-20 10:11:47 +0200

    Add script for starting a new project

---
+++ scripts/start-new-project.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 project-slug destination-folder"
+  exit
+fi
+
+projectslug=$1
+destinationfolder=$2
+
+foundnvm=false
+
+if [ -f ~/.nvm/nvm.sh ]; then
+  source ~/.nvm/nvm.sh
+  foundnvm=true
+elif [ $(which brew) ]; then
+  source $(brew --prefix nvm)/nvm.sh
+  foundnvm=true
+fi
+
+mkdir -p $destinationfolder
+git archive -o $destinationfolder/head.zip -9 HEAD
+cd $destinationfolder
+unzip head.zip
+rm head.zip
+git init
+
+sed -i -e "s/\"antoinette\"/\"$projectslug\"/g" "package.json"
+
+if [ $foundnvm ]; then
+  nvm install
+  nvm use
+  npm install
+fi
