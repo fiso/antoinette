ed4ddea37968bed5905680b8cd4ce6d2079b2936
fiso <filip.l.haglund@gmail.com>
2020-07-15 17:25:01 +0200

    Bail out nicely if Redis is not up

--- src/php/antoinette/cache_hooks.php
+++ src/php/antoinette/cache_hooks.php
@@ -5,9 +5,14 @@ function flush_redis() {
         return;
     }
 
-    $redis = new Redis();
-    $redis->connect('127.0.0.1');
-    $redis->flushAll();
+    try {
+        $redis = new Redis();
+        $redis->connect('127.0.0.1');
+        $redis->flushAll();
+    } catch (Exception $e) {
+        error_log('Redis unavailable');
+        error_log($e);
+    }
 }
 add_action('save_post', 'flush_redis');
 add_action('updated_option', 'flush_redis');
